5293db73d83c7d450ff2a834f0b9ad91e697aee1
Mikeil Akovity (1iekim) <mikeil98@mail.ru>
2023-07-20 23:20:20 +0300

    feat: create react app

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Redirect, Routes } from "react-router-dom";
+import { authRoutes, publicRoutes } from "../routes";
+
+const AppRouter = () => {
+  const isAuth = false;
+  return (
+    <Routes>
+      {isAuth && authRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} Component={Component} exact />
+      ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} Component={Component} exact />
+      ))}
+    </Routes>
+  );
+};
+
+export default AppRouter;
